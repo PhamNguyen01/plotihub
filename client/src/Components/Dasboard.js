ddc124f032bb8e74b7fded92c13397c33b54f393
carlton <carltonmbaja0@gmail.com>
2022-10-12 00:54:05 +0300

    Dashboard halfway done

---
+++ client/src/Components/Dasboard.js
@@ -0,0 +1,120 @@
+import * as React from 'react';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import '../App.css'
+import WifiOutlinedIcon from '@mui/icons-material/WifiOutlined';
+import SignalWifiBadOutlinedIcon from '@mui/icons-material/SignalWifiBadOutlined';
+import AccountBalanceWalletIcon from '@mui/icons-material/AccountBalanceWallet';
+import AccountBalanceIcon from '@mui/icons-material/AccountBalance';
+import ResponsiveDialog from './Modal';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { useTheme } from '@mui/material/styles';
+
+export default function BasicButtons() {
+    const [open, setOpen] = React.useState(false);
+    const theme = useTheme();
+    const fullScreen = useMediaQuery(theme.breakpoints.down('md'));
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+    return (
+        <div>
+ 
+               <Stack className='buttons' spacing={2} direction="row">
+                <ResponsiveDialog />
+
+                <div>
+                    <Button className='button' variant="outlined" onClick={handleClickOpen}>
+                        Record Payment
+                    </Button>
+                    <Dialog
+                        fullScreen={fullScreen}
+                        open={open}
+                        onClose={handleClose}
+                        aria-labelledby="responsive-dialog-title"
+                    >
+                        <DialogTitle id="responsive-dialog-title">
+                            {"Send Balance Reminders"}
+                        </DialogTitle>
+                        <DialogContent>
+                            <DialogContentText>
+                              <p>Select Tenant</p>  <input placeholder='Select Tenant'></input>
+                              <p>Paid Amount</p>  <input placeholder='Enter Paid Amount e.g 10000'></input>
+                              <p>Select Tenant</p>  <input placeholder='Select Tenant'></input>
+                              <p>Payment Type (optional)</p>  <input placeholder='Select Tenant'></input>
+                              <p>Description (optional)</p>  <input placeholder='Select Tenant'></input>
+                                {/* <Button variant="outlined">Add PAyment</Button> */}
+                            </DialogContentText>
+                        </DialogContent>
+                        <DialogActions>
+                        <Button variant="outlined" onClick={handleClose} autoFocus>Add Payment</Button>
+
+                            {/* <Button autoFocus onClick={handleClose}>
+                                Disagree
+                            </Button>
+                            <Button onClick={handleClose} autoFocus>
+                                Agree
+                            </Button> */}
+                        </DialogActions>
+                    </Dialog>
+                </div>
+                {/* <Button onClick={handleClickOpen} className='button' variant="outlined">Record Payment</Button> */}
+
+                <Button className='button' variant="outlined">Add Utilities</Button>
+            </Stack>
+
+            <div className='card'>
+                <div className='cards'>
+                    <div className='card-details'>
+                        <div className='card-info'>
+                            <WifiOutlinedIcon sx={{ fontSize: 60 }} />
+                            <p>Unpaid Tenant Balance</p>
+                            <h3>10.00 KSH</h3>
+                            <p>Total</p>
+                        </div>
+
+                    </div >
+                    <div className='card-details'>
+                        <div className='card-info'>
+                            <SignalWifiBadOutlinedIcon sx={{ fontSize: 60 }} />
+                            <p>Payments</p>
+                            <h3>0 KSH</h3>
+                            <p>October</p>
+                        </div>
+
+                    </div >
+                    <div className='card-details'>
+                        <div className='card-info'>
+                            <AccountBalanceWalletIcon sx={{ fontSize: 60 }} />
+                            <p>Total Vacancies</p>
+                            <h3> 0</h3>
+                            <p>0 Occupied</p>
+                        </div>
+
+                    </div >
+
+                    <div className='card-details'>
+                        <div className='card-info'>
+                            <AccountBalanceIcon sx={{ fontSize: 60 }} />
+                            <p>Billing Address</p>
+                            <h3>Bank Acc</h3>
+                        </div>
+
+                    </div >
+                </div>
+
+            </div>
+        </div>
+    );
+}
+
--- client/src/Components/Drawer.js
+++ client/src/Components/Drawer.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import '../App.css'
 import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
 import CssBaseline from '@mui/material/CssBaseline';
@@ -7,102 +8,79 @@ import Toolbar from '@mui/material/Toolbar';
 import List from '@mui/material/List';
 import Typography from '@mui/material/Typography';
 import Divider from '@mui/material/Divider';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
+import PaidIcon from '@mui/icons-material/Paid';
+import NotificationsActiveIcon from '@mui/icons-material/NotificationsActive';
+import DashboardIcon from '@mui/icons-material/Dashboard';
 
-const drawerWidth = 240;
+const drawerWidth = 242;
 
 export default function PermanentDrawerLeft() {
-  return (
-    <Box sx={{ display: 'flex' }}>
-      <CssBaseline />
-      <AppBar
-        position="fixed"
-        sx={{ width: `calc(100% - ${drawerWidth}px)`, ml: `${drawerWidth}px` }}
-      >
-        <Toolbar>
-          <Typography variant="h6" noWrap component="div">
-            Permanent drawer
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <Drawer
-        sx={{
-          width: drawerWidth,
-          flexShrink: 0,
-          '& .MuiDrawer-paper': {
-            width: drawerWidth,
-            boxSizing: 'border-box',
-          },
-        }}
-        variant="permanent"
-        anchor="left"
-      >
-        <Toolbar />
-        <Divider />
-        <List>
-          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-            <ListItem key={text} disablePadding>
-              <ListItemButton>
-                <ListItemIcon>
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                </ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItemButton>
-            </ListItem>
-          ))}
-        </List>
-        <Divider />
-        <List>
-          {['All mail', 'Trash', 'Spam'].map((text, index) => (
-            <ListItem key={text} disablePadding>
-              <ListItemButton>
-                <ListItemIcon>
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                </ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItemButton>
-            </ListItem>
-          ))}
-        </List>
-      </Drawer>
-      <Box
-        component="main"
-        sx={{ flexGrow: 1, bgcolor: 'background.default', p: 3 }}
-      >
-        <Toolbar />
-        <Typography paragraph>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-          tempor incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non
-          enim praesent elementum facilisis leo vel. Risus at ultrices mi tempus
-          imperdiet. Semper risus in hendrerit gravida rutrum quisque non tellus.
-          Convallis convallis tellus id interdum velit laoreet id donec ultrices.
-          Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-          adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra
-          nibh cras. Metus vulputate eu scelerisque felis imperdiet proin fermentum
-          leo. Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt lobortis
-          feugiat vivamus at augue. At augue eget arcu dictum varius duis at
-          consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa
-          sapien faucibus et molestie ac.
-        </Typography>
-        <Typography paragraph>
-          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper
-          eget nulla facilisi etiam dignissim diam. Pulvinar elementum integer enim
-          neque volutpat ac tincidunt. Ornare suspendisse sed nisi lacus sed viverra
-          tellus. Purus sit amet volutpat consequat mauris. Elementum eu facilisis
-          sed odio morbi. Euismod lacinia at quis risus sed vulputate odio. Morbi
-          tincidunt ornare massa eget egestas purus viverra accumsan in. In hendrerit
-          gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem
-          et tortor. Habitant morbi tristique senectus et. Adipiscing elit duis
-          tristique sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
-          eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
-          posuere sollicitudin aliquam ultrices sagittis orci a.
-        </Typography>
-      </Box>
-    </Box>
-  );
+    return (
+        <Box sx={{ display: 'flex' }}>
+            <CssBaseline />
+            <AppBar
+                position="fixed"
+                sx={{ width: `calc(100% - ${drawerWidth}px)`, ml: `${drawerWidth}px` }}
+            >
+                <Toolbar>
+                    <Typography variant="h6" noWrap component="div">
+                        Dashboard
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <Drawer
+                sx={{
+                    width: drawerWidth,
+                    flexShrink: 0,
+                    '& .MuiDrawer-paper': {
+                        width: drawerWidth,
+                        boxSizing: 'border-box',
+                    },
+                }}
+                variant="permanent"
+                anchor="left"
+            >
+                <Toolbar />
+                <Divider />
+                <List >
+                    <p className='drawer'> Todo</p>
+                    <p className='drawer'> Dashboard</p>
+                    <p className='drawer'> Financials</p>
+                    <p className='drawer'>Invoices</p>
+                    <p className='drawer'>Payments</p>
+                    <p className='drawer'>Expenses</p>
+                    <p className='drawer'>Tenants</p>
+                    <p className='drawer'>Properties</p>
+                    <p className='drawer'>Property/Unit</p>
+                    <p className='drawer'>Utilities</p>
+                    <p className='drawer'>Maintenance</p>
+                    <p className='drawer'>Reports</p>
+                    <p className='drawer'>Communication</p>
+                    <p className='drawer'>Settings</p>
+                </List>
+                {/* <List>
+                    {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+                        <ListItem key={text} disablePadding>
+                            <ListItemButton>
+                                <ListItemIcon>
+                                    {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                                </ListItemIcon>
+                                
+                                <ListItemText primary={text} />
+                            </ListItemButton>
+                        </ListItem>
+                    ))}
+                </List> */}
+                <Divider />
+               
+            </Drawer>
+            <Box
+                component="main"
+                sx={{ flexGrow: 1, bgcolor: 'background.default', p: 3 }}
+            >
+                <Toolbar />
+             
+            </Box>
+        </Box>
+    );
 }
